d7b890f40c4f237222465c4e9f74f14b739d8bde
Naveen Roy <naveenroy001@gmail.com>
2021-10-22 14:05:38 +0530

    update

--- config/sequelize.js
+++ config/sequelize.js
@@ -15,12 +15,5 @@ const sequelize = new Sequelize(process.env.DBNAME, 'root', process.env.PASSWORD
     }
 });
 
-// try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-// } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
-
 
 module.exports = sequelize
